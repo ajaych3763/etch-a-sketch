c26badea515f5c54ef6da0046180c7099795eb69
ajaych3763 <ajaych3763@gmail.com>
2025-07-19 20:45:42 +0530

    Add event listenter for the button and event listener for all the new cells created after clicking the button

--- .js
+++ .js
@@ -9,10 +9,32 @@ for (let i = 0; i < gridDimension * gridDimension; i++) {
 }
 
 const gridCells = document.querySelectorAll(".grid-cell");
-// console.log(Array.from(gridCell));
+//console.log(Array.from(gridCell));
 gridCells.forEach(element => {
     element.addEventListener("mouseenter", (Event) => {
         let target = Event.target;
         target.style.backgroundColor = "red";
     })
+});
+
+let resizeBtn = document.querySelector("button");
+resizeBtn.addEventListener('click', () => {
+    let gridDimension = prompt("Enter new Grid Dimension (Max : 100) : ");
+    gridDimension = Math.min(100, gridDimension);
+    const gridCells = document.querySelectorAll(".grid-cell");
+    div.innerHTML = '';
+    for (let i = 0; i < gridDimension * gridDimension; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid-cell");
+        grid.style.height = `${600/gridDimension}px`;
+        grid.style.width = `${900/gridDimension}px`;
+        div.appendChild(grid);
+    }
+    const newGridCells = document.querySelectorAll(".grid-cell");
+    newGridCells.forEach(element => {
+        element.addEventListener("mouseenter", (Event) => {
+            let target = Event.target;
+            target.style.backgroundColor = "red";
+        })
+    });
 });
