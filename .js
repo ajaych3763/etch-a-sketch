66ba1c328dc913d145da2185c5b68a624cf6c24a
ajaych3763 <ajaych3763@gmail.com>
2025-07-19 19:09:03 +0530

    create grid containers, add height and width as per the container and grid dimension

--- .js
+++ .js
@@ -1,7 +1,10 @@
 const div = document.querySelector(".container");
-for (let i = 0; i < 256; i++) {
+let gridDimension = 16;
+for (let i = 0; i < gridDimension * gridDimension; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid-cell");
+    grid.style.height = `${600/gridDimension}px`;
+    grid.style.width = `${900/gridDimension}px`;
     div.appendChild(grid);
 }
 
